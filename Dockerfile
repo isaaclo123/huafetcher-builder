5abb411bb56059d45abf70cbc6f0a32f23b453fe
isaaclo123 <isaaclo123@gmail.com>
2026-01-16 21:48:37 -0600

    work

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM --platform=linux/amd64 ubuntu
+FROM --platform=linux/amd64 ubuntu:22.04
 #FROM ubuntu
 
 # Disable download progress logging for buildozer
@@ -6,25 +6,14 @@ ENV CI=1
 
 RUN set -eux \
   ; apt-get update -yqq \
-  ; DEBIAN_FRONTEND=noninteractive apt-get install -yqq --no-install-recommends \
-    autoconf \
-    automake \
-    autotools-dev \
-    build-essential \
-    default-jdk \
-    git \
-    libcrypto++-dev \
-    libffi-dev \
-    libltdl-dev \
-    libssl-dev \
-    libtool \
-    python3 \
-    python3-dev \
-    python3-pip \
-    sed \
-    unzip \
-    zip \
-    zlib1g-dev \
+  ; DEBIAN_FRONTEND=noninteractive apt-get install -y \
+    git zip unzip openjdk-17-jdk python3-pip autoconf libtool pkg-config zlib1g-dev libncurses5-dev libncursesw5-dev libtinfo5 cmake libffi-dev libssl-dev \
   ; useradd -ms /bin/bash buildozer
+
+RUN set -eux \
+  ; pip3 install \
+    buildozer \
+    kivy \
+    Cython==0.29.33
 
 USER buildozer
--- build.sh
+++ build.sh
@@ -1,32 +1,35 @@
-#!/bin/bash -ex
+j!/bin/bash -ex
 
 mkdir -p build
 chmod 777 build
+mkdir -p huafetcher-builder-buildozer
+chmod 777 huafetcher-builder-buildozer
 docker rmi huafetcher-builder || true
 docker build -t huafetcher-builder .
 docker run -it \
   --platform linux/amd64 \
-  -v huafetcher-builder-buildozer:/home/buildozer \
+  -v $PWD/huafetcher-builder-buildozer:/home/buildozer \
   -v $PWD/build/:/build \
   huafetcher-builder \
   bash -c 'set -eux \
-    ; pip install \
-      buildozer \
-      Cython \
-      kivy \
     ; export PATH=/home/buildozer/.local/bin:$PATH \
     ; cd /home/buildozer \
     ; if [ -e huafetcher ] \
     ; then \
         cd huafetcher \
-    ;   git checkout master \
-    ;   git reset --hard \
     ;   git config pull.rebase true \
     ;   git pull \
     ; else \
-        git clone https://codeberg.org/rusi/huafetcher.git \
+        git clone --recurse-submodules https://codeberg.org/isaaclo123/huafetcher.git \
     ;   cd huafetcher \
+    ;   touch huami_token/__init__.py \
     ; fi \
     ; buildozer -v android debug \
     ; cp /home/buildozer/huafetcher/bin/*.apk /build/ \
   '
+
+
+# ;   git checkout master \
+# ;   git reset --hard \
+# ;   git config pull.rebase true \
+# ;   git pull \
